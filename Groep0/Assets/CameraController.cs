946031299762ebe6728e5296e6b0f0123de3988b
Redwane Boulakhrif <boulakhrifprive@hotmail.com>
2021-06-05 23:03:57 +0200

    karakter klaar

---
+++ Groep0/Assets/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public SpelerController player;
+    private float sens = 200f;
+    private float clamp = 85f;
+
+    private float verticalRotation;
+    private float horizontalRotation;
+
+    private void Start()
+    {
+        this.verticalRotation = this.transform.localEulerAngles.x;
+        this.horizontalRotation = this.transform.eulerAngles.y;
+    }
+
+    private void Update()
+    {
+        Look();
+        Debug.DrawRay(this.transform.position, this.transform.forward * 2, Color.red);
+    }
+
+    private void Look()
+    {
+        float mouseVertical = -Input.GetAxis("Mouse Y");
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+
+        this.verticalRotation += mouseVertical * this.sens * Time.deltaTime;
+        this.horizontalRotation += mouseHorizontal * this.sens * Time.deltaTime;
+
+        this.verticalRotation = Mathf.Clamp(this.verticalRotation, -this.clamp, this.clamp);
+
+        this.transform.localRotation = Quaternion.Euler(this.verticalRotation, 0f, 0f);
+        this.player.transform.rotation = Quaternion.Euler(0f, this.horizontalRotation, 0f);
+    }
+}
---
+++ Groep0/Assets/SpelerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpelerController : MonoBehaviour
+{
+    public Animator anim;
+    private Rigidbody rb;
+    public LayerMask layerMask;
+    public bool ground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        Grounded();
+        Jump();
+        Move();
+    }
+
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && this.ground)
+        {
+            this.rb.AddForce(Vector3.up * 4, ForceMode.Impulse);
+        }
+    }
+
+    private void Grounded()
+    {
+        if (Physics.CheckSphere(this.transform.position + Vector3.down, 0.2f, layerMask))
+        {
+            this.ground = true;
+        }
+        else
+        {
+            this.ground = false;
+        }
+
+        this.anim.SetBool("jump", !this.ground);
+    }
+    private void Move()
+    {
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        Vector3 movement = this.transform.forward * verticalAxis + this.transform.right * horizontalAxis;
+
+        movement.Normalize();
+
+        this.transform.position += movement * 0.04f;
+
+        this.anim.SetFloat("vertical", verticalAxis);
+        this.anim.SetFloat("horizontal", horizontalAxis);
+    }
+}
